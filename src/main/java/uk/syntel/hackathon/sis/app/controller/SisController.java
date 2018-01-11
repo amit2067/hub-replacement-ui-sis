d322c9e591983c884563bf78512bd4738988ca70
amit2067 <amit2067@gmail.com>
2018-01-11 10:00:03 +0000

    Update SisController.java

--- src/main/java/uk/syntel/hackathon/sis/app/controller/SisController.java
+++ src/main/java/uk/syntel/hackathon/sis/app/controller/SisController.java
@@ -8,36 +8,49 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import uk.syntel.hackathon.sis.app.beans.Application;
 import uk.syntel.hackathon.sis.app.beans.Customer;
 import uk.syntel.hackathon.sis.app.service.SisService;
 
+@Api(value="SIS service", description="SIS Operations to deal with Customers and Applications")
 @RestController
 public class SisController {
 
 	@Autowired
-	SisService service;
+	private SisService service;
 
+	@ApiOperation(value = "Retrieve list of customers",response = Iterable.class)
 	@GetMapping("/getCustomers")
 	public List<Customer> getCustomers() {
 		List<Customer> customers = service.getCustomers();
 		return customers;
 	}
 
+	@ApiOperation(value = "Retrieve list of applications",response = Iterable.class)
 	@GetMapping("/getApplications")
 	public List<Application> getApplications() {
 		  List<Application> applications = service.getApplications();
           return applications;
 	}
 	
+	@ApiOperation(value = "Update details of Customer",response = Customer.class)
 	@PostMapping("/updateCustomer")
 	public Customer updateCustomer(@RequestBody Customer customer) {
 		return service.updateCustomer(customer);
 	}
 	
+	@ApiOperation(value = "Update details of Application",response = Application.class)
 	@PostMapping("/updateApplication")
 	public Application updateApplication(@RequestBody Application application) {
 		return service.updateApplication(application);
+	}
+		
+	@ApiOperation(value = "Confirm attendance against application",response = Application.class)
+	@PostMapping("confirmAttendance")
+	public Application confirmAttendance(@RequestBody Application application) {
+		return service.confirmAttendance(application);
 	}
 
 }
