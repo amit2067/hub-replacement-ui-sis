4d30f193c40ecd5e74f95a1565e0c38739f6fda0
amit2067 <amit2067@gmail.com>
2018-01-11 10:01:15 +0000

    Update SisService.java

--- src/main/java/uk/syntel/hackathon/sis/app/service/SisService.java
+++ src/main/java/uk/syntel/hackathon/sis/app/service/SisService.java
@@ -2,22 +2,30 @@ package uk.syntel.hackathon.sis.app.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import uk.syntel.hackathon.sis.app.beans.Application;
 import uk.syntel.hackathon.sis.app.beans.Customer;
 import uk.syntel.hackathon.sis.app.repository.ApplicationRepository;
 import uk.syntel.hackathon.sis.app.repository.CustomerRepository;
+import uk.syntel.hackathon.sis.app.util.SisConstants;
 
 @Service
 public class SisService {
 
+	Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	private CustomerRepository customerRepository;
 
 	@Autowired
 	private ApplicationRepository appRepository;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 	
 	public List<Customer> getCustomers() {
 		return customerRepository.findAll();
@@ -33,6 +41,39 @@ public class SisService {
 	
 	public Application updateApplication(Application application) {
 		return appRepository.save(application);
+	}
+	
+	public Customer saveCustomer(Customer customer) {
+		Customer createdCustomer = customerRepository.save(customer);
+		logger.info("SIS : Customer propagated via Kafka - "+customer);
+		return createdCustomer;
+	}
+	
+	public Application saveApplication(Application application) {
+		Application createdApp = appRepository.save(application);
+		logger.info("SIS : Application updated :"+createdApp);
+		if (null !=createdApp && null != createdApp.getId()) {
+			String result = restTemplate.postForObject(SisConstants.APPLICATION_PUBLISH_URL, createdApp, String.class);
+			logger.info("SIS : Application publish to Kafka status :"+result);
+		}
+		return createdApp;
+	}
+
+	public void propogateCustomer(Customer customer) {
+		saveCustomer(customer);
+	}
+	
+	public void propogateApplication(Application application) {
+		if (SisConstants.SIS.equalsIgnoreCase(application.getCreatedBy())) {
+			logger.info("SIS : Application created by self - not inserting");
+			return;
+		}
+		Application propogatedApp = appRepository.save(application);
+		logger.info("CLASS : Application propagated via Kafka - "+propogatedApp);
+	}
+
+	public Application confirmAttendance(Application application) {
+		return saveApplication(application);
 	}
 
 }
