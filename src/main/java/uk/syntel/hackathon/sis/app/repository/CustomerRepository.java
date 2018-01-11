e56b0e1f53089ecc94f02ef9d76b6e26df4f42f6
amit2067 <amit2067@gmail.com>
2018-01-11 10:00:45 +0000

    Update CustomerRepository.java

--- src/main/java/uk/syntel/hackathon/sis/app/repository/CustomerRepository.java
+++ src/main/java/uk/syntel/hackathon/sis/app/repository/CustomerRepository.java
@@ -2,13 +2,13 @@ package uk.syntel.hackathon.sis.app.repository;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import uk.syntel.hackathon.sis.app.beans.Customer;
 
 @Repository
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
+public interface CustomerRepository extends MongoRepository<Customer, Long> {
 
 	List<Customer> findAll();
 
